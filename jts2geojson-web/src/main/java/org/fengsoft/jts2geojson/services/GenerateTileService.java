37413f5f64f7d7a8f2af4dc130a20c4b565765f8
polixiaohai <zhangjunfeng0720@gmail.com>
2018-09-05 21:30:50 +0800

    多线程下载天地图

--- jts2geojson-web/src/main/java/org/fengsoft/jts2geojson/services/GenerateTileService.java
+++ jts2geojson-web/src/main/java/org/fengsoft/jts2geojson/services/GenerateTileService.java
@@ -4,6 +4,7 @@ import cn.com.enersun.dgpmicro.common.GlobalMercator;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.beetl.sql.core.SQLManager;
+import org.fengsoft.jts2geojson.common.TileIndex;
 import org.fengsoft.jts2geojson.common.TileType;
 import org.locationtech.jts.geom.Envelope;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.util.StringUtils;
 import javax.imageio.stream.FileImageOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
@@ -39,10 +42,19 @@ public class GenerateTileService {
     private AtomicInteger count = new AtomicInteger();// 总数 原子操作
     private AtomicInteger failCount = new AtomicInteger();// 总数 原子操作
     private volatile boolean isRunning = true;
+    private volatile boolean isOverwrite = false;
+    private volatile TileType tileType;
+    private volatile String tileName;
+    private volatile boolean isOver = false;
+
+    private volatile BlockingQueue<TileIndex> queue = new LinkedBlockingDeque<>(30);// 内存缓冲区
     private Envelope envelope;
 
     public void run(String tileName, Envelope envelope, String epsg, TileType tileType, Boolean isOverwrite) {
+        this.tileName = tileName;
         this.envelope = envelope;
+        this.tileType = tileType;
+        this.isOverwrite = isOverwrite;
         File file = new File(imageTilePath);
         if (!file.exists()) file.mkdirs();
         File subDir = new File(imageTilePath, tileName);
@@ -60,62 +72,69 @@ public class GenerateTileService {
                 File xFile = new File(parent, String.valueOf(tx));
                 if (!xFile.exists()) xFile.mkdir();
                 for (int ty = tmaxxy[1]; ty > tminxy[1] - 1; ty--) {
-                    File targetFile = new File(xFile, String.format("%d.%s", ty, "png"));
                     if (isRunning) {
-                        String url = "";
-                        int[] gootleXY = mercator.googleTile(tx, ty, tz);
-                        if (tileType.getType().equals(TileType.BING.getType())) {
-                            String code = mercator.tileXYToQuadKey(gootleXY[0], gootleXY[1], tz);
-                            url = String.format(tileType.getUrl(), code);
-                        } else if (tileType.getType().equals(TileType.GOOGLE.getType())) {
-                            url = String.format(tileType.getUrl(), tz, gootleXY[0], gootleXY[1]);
-                        } else if (tileType.getType().equals(TileType.OSM.getType())) {
-                            url = String.format(tileType.getUrl(), tz, tx, ty);
-                        } else if (tileType.getType().equals(TileType.TDTCVR.getType())) {
-                            url = String.format(tileType.getUrl(), gootleXY[0] % 6, gootleXY[0], gootleXY[1], tz);
-                        } else if (tileType.getType().equals(TileType.TDTVEC.getType())) {
-                            url = String.format(tileType.getUrl(), gootleXY[0] % 6, gootleXY[0], gootleXY[1], tz);
-                        } else if (tileType.getType().equals(TileType.GOOGLEIMAGE.getType())) {
-                            url = String.format(tileType.getUrl(), gootleXY[0], gootleXY[1], tz);
-                        }
-                        if (!StringUtils.isEmpty(url)) {
-                            if (!isOverwrite && targetFile.exists()) {
-                                return;
-                            }
-                            Request request = new Request.Builder().url(url).build();
-
-                            Call call = okHttpClient.newCall(request);
-                            int finalTx = tx;
-                            int finalTy = ty;
-                            int finalTz = tz;
-                            call.enqueue(new Callback() {
-                                @Override
-                                public void onFailure(Call call, IOException e) {
-                                    log.info(String.format("error tile:%d-%d-%d：%s", finalTx, finalTy, finalTz, e.getMessage()));
-                                    failCount.getAndIncrement();
-                                }
-
-                                @Override
-                                public void onResponse(Call call, Response response) throws IOException {
-                                    synchronized (this) {
-                                        //得到从网上获取资源，转换成我们想要的类型
-                                        byte[] data = response.body().bytes();
-
-                                        try {
-                                            FileImageOutputStream imageOutput = new FileImageOutputStream(targetFile);
-                                            imageOutput.write(data, 0, data.length);
-                                            imageOutput.close();
-                                            count.getAndIncrement();
-                                        } catch (Exception ex) {
-                                            ex.printStackTrace();
-                                        }
-                                    }
-                                }
-                            });
-
-                        }
+                        queue.add(new TileIndex(tx, ty, tz));
                     }
                 }
+            }
+        }
+        isOver = true;
+    }
+
+    public void download() {
+        while (true) {
+            TileIndex tileIndex = queue.poll();
+            if (tileIndex == null && isOver) {
+                break;
+            }
+            File targetFile = new File(new File(imageTilePath, tileName + File.separator + tileIndex.getZ() + File.separator + tileIndex.getX()), String.format("%d.%s", tileIndex.getY(), "png"));
+            String url = "";
+            int[] gootleXY = mercator.googleTile(tileIndex.getX(), tileIndex.getY(), tileIndex.getZ());
+            if (tileType.getType().equals(TileType.BING.getType())) {
+                String code = mercator.tileXYToQuadKey(gootleXY[0], gootleXY[1], tileIndex.getZ());
+                url = String.format(tileType.getUrl(), code);
+            } else if (tileType.getType().equals(TileType.GOOGLE.getType())) {
+                url = String.format(tileType.getUrl(), tileIndex.getZ(), gootleXY[0], gootleXY[1]);
+            } else if (tileType.getType().equals(TileType.OSM.getType())) {
+                url = String.format(tileType.getUrl(), tileIndex.getZ(), tileIndex.getX(), tileIndex.getY());
+            } else if (tileType.getType().equals(TileType.TDTCVR.getType())) {
+                url = String.format(tileType.getUrl(), gootleXY[0] % 6, gootleXY[0], gootleXY[1], tileIndex.getZ());
+            } else if (tileType.getType().equals(TileType.TDTVEC.getType())) {
+                url = String.format(tileType.getUrl(), gootleXY[0] % 6, gootleXY[0], gootleXY[1], tileIndex.getZ());
+            } else if (tileType.getType().equals(TileType.GOOGLEIMAGE.getType())) {
+                url = String.format(tileType.getUrl(), gootleXY[0], gootleXY[1], tileIndex.getZ());
+            }
+            if (!StringUtils.isEmpty(url)) {
+                if (!isOverwrite && targetFile.exists()) {
+                    return;
+                }
+                Request request = new Request.Builder().url(url).build();
+
+                Call call = okHttpClient.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        log.info(String.format("error tile:%d-%d-%d：%s", tileIndex.getX(), tileIndex.getY(), tileIndex.getZ(), e.getMessage()));
+                        failCount.getAndIncrement();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        synchronized (this) {
+                            //得到从网上获取资源，转换成我们想要的类型
+                            byte[] data = response.body().bytes();
+
+                            try {
+                                FileImageOutputStream imageOutput = new FileImageOutputStream(targetFile);
+                                imageOutput.write(data, 0, data.length);
+                                imageOutput.close();
+                                count.getAndIncrement();
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                });
             }
         }
     }
